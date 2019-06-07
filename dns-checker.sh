f1c77b5e2071e1a2fd0c5e1df9ed0af19396f680
Christian Hernandez <c_hernandez1982@yahoo.com>
2019-06-06 19:31:59 -0700

    fixed the script

--- dns-checker.sh
+++ dns-checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 domain=$1
 clusterid=$2
-if [[ $* -eq 0 ]]; then
+if [[ ${#domain} -eq 0 -o ${#clusterid} -eq 0 ]]; then
   echo "Usage: $(basename $0) <domain> <clusterid>"
   exit
 fi
