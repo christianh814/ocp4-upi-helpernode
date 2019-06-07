66f2e16621078efa2bde099cce67724312ed04a9
Christian Hernandez <c_hernandez1982@yahoo.com>
2019-06-06 19:30:40 -0700

    added dns checker

---
+++ dns-checker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+domain=$1
+clusterid=$2
+if [[ $* -eq 0 ]]; then
+  echo "Usage: $(basename $0) <domain> <clusterid>"
+  exit
+fi
+#
+echo ""
+for name in m{1..3} w{1..3} boostrap etcd-{0..2} foo.apps api api-int
+do
+  echo -n "${name}.${clusterid}.${domain}	"
+  dig @localhost ${name}.${clusterid}.${domain} +short
+done
+dig @localhost _etcd-server-ssl._tcp.${clusterid}.${domain} SRV +short
+echo ""
+##
+##
