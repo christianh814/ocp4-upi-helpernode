b41f6b54a5b0f96176d01d5691f3ecb94154d17d
Christian Hernandez <c_hernandez1982@yahoo.com>
2019-06-06 19:43:40 -0700

    rev

--- dns-checker.sh
+++ dns-checker.sh
@@ -6,12 +6,22 @@ if [[ ${#domain} -eq 0 ]] || [[ ${#clusterid} -eq 0 ]]; then
   exit
 fi
 #
-echo ""
+echo "Foward lookup"
+echo "============="
 for name in m{1..3} w{1..3} boostrap etcd-{0..2} foo.apps api api-int
 do
   echo -n "${name}.${clusterid}.${domain}				"
   dig @localhost ${name}.${clusterid}.${domain} +short
 done
+echo "Reverse lookup"
+echo "=============="
+for name in m{1..3} w{1..3} boostrap etcd-{0..2} foo.apps api api-int
+do
+  echo -n "$(dig @localhost ${name}.${clusterid}.${domain} +short)                               "
+  dig @localhost -x $(dig @localhost ${name}.${clusterid}.${domain} +short) +short
+done
+echo "SRV Records"
+echo "==========="
 dig @localhost _etcd-server-ssl._tcp.${clusterid}.${domain} SRV +short
 echo ""
 ##
