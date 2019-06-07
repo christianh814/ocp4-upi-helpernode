6b4bd999987a785cefac61ea1376828386615332
Christian Hernandez <c_hernandez1982@yahoo.com>
2019-06-06 19:37:26 -0700

    AHHHHHHHHHHHHHHHHHHH

--- dns-checker.sh
+++ dns-checker.sh
@@ -9,7 +9,7 @@ fi
 echo ""
 for name in m{1..3} w{1..3} boostrap etcd-{0..2} foo.apps api api-int
 do
-  echo -n "${name}.${clusterid}.${domain}		"
+  echo -n "${name}.${clusterid}.${domain}				"
   dig @localhost ${name}.${clusterid}.${domain} +short
 done
 dig @localhost _etcd-server-ssl._tcp.${clusterid}.${domain} SRV +short
